969d129b0a2a28a1195bb016815ff4a78abdf83e
albertusadrian7 <albertsadrian7@gmail.com>
2021-09-25 23:46:55 +0700

    third commit: fix fitur edit buku

--- app/src/main/java/android/kotlin/bookstore/EditBukuActivity.kt
+++ app/src/main/java/android/kotlin/bookstore/EditBukuActivity.kt
@@ -26,44 +26,42 @@ class EditBukuActivity : AppCompatActivity() {
         inputEditPenulis.setText(intent.getStringExtra("rating"))
         inputEditRating.setText(intent.getStringExtra("harga"))
         btnEdit.setOnClickListener {
-            btnEdit.setOnClickListener {
-                RetrofitClient.instance.updateBuku(
-                    inputId.text.toString().trim(),
-                    inputJudul.text.toString().trim(),
-                    inputPenulis.text.toString().trim(),
-                    inputRating.text.toString().trim(),
-                    inputHarga.text.toString().trim(),
-                    "update_buku"
-                ).enqueue(object : Callback<DefaultResponse> {
-                    override fun onResponse(
-                        call: Call<DefaultResponse>,
-                        response: Response<DefaultResponse>
-                    ) {
-                        if (response!!.isSuccessful) {
-                            if (response.body()?.status == 1) {
-                                Toast.makeText(
-                                    this@EditBukuActivity,
-                                    "Berhasil Edit Buku",
-                                    Toast.LENGTH_LONG
-                                ).show()
-                            } else {
-                                Toast.makeText(
-                                    this@EditBukuActivity,
-                                    "Gagal edit buku",
-                                    Toast.LENGTH_LONG
-                                ).show()
-                            }
+            RetrofitClient.instance.updateBuku(
+                inputId.text.toString().trim(),
+                inputJudul.text.toString().trim(),
+                inputPenulis.text.toString().trim(),
+                inputRating.text.toString().trim(),
+                inputHarga.text.toString().trim(),
+                "update_buku"
+            ).enqueue(object : Callback<DefaultResponse> {
+                override fun onResponse(
+                    call: Call<DefaultResponse>,
+                    response: Response<DefaultResponse>
+                ) {
+                    if (response!!.isSuccessful) {
+                        if (response.body()?.status == 1) {
+                            Toast.makeText(
+                                this@EditBukuActivity,
+                                "Berhasil Edit Buku",
+                                Toast.LENGTH_LONG
+                            ).show()
+                        } else {
+                            Toast.makeText(
+                                this@EditBukuActivity,
+                                "Gagal edit buku",
+                                Toast.LENGTH_LONG
+                            ).show()
                         }
                     }
-                    override fun onFailure(call: Call<DefaultResponse>, t: Throwable) {
-                        Toast.makeText(
-                            this@EditBukuActivity,
-                            "Tidak ada respon",
-                            Toast.LENGTH_LONG
-                        ).show()
-                    }
-                })
-            }
+                }
+                override fun onFailure(call: Call<DefaultResponse>, t: Throwable) {
+                    Toast.makeText(
+                        this@EditBukuActivity,
+                        "Tidak ada respon",
+                        Toast.LENGTH_LONG
+                    ).show()
+                }
+            })
         }
     }
 
